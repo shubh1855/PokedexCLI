43ee9aa3108012c28410bff6fa27d8e76d826742
shubh1855 <paulshubhranil1855@gmail.com>
2026-05-07 12:39:38 +0530

    added repl logic with some basic command and command structs to make it extensible

--- repl.go
+++ repl.go
@@ -7,30 +7,81 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
+
 		reader.Scan()
 
+		if err := reader.Err(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
 		words := cleanInput(reader.Text())
+
 		if len(words) == 0 {
 			continue
 		}
 
 		commandName := words[0]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		command, exists := commands[commandName]
+
+		if exists {
+			if err := command.callback(); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 }
 
 func cleanInput(text string) []string {
-	lowered := strings.ToLower(text)
-	trimmed := strings.TrimSpace(lowered)
+	return strings.Fields(strings.ToLower(text))
+}
 
-	if trimmed == "" {
-		return []string{}
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 
-	return strings.Fields(trimmed)
+	return nil
 }
