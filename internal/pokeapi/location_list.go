e1362ca0a08bb7fcadee056ded2558e239919808
shubh1855 <paulshubhranil1855@gmail.com>
2026-05-08 13:22:57 +0530

    updated all the files for using caching

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -6,11 +6,22 @@ import (
 	"net/http"
 )
 
-// ListLocations -
 func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	url := baseURL + "/location-area"
+
 	if pageURL != nil {
 		url = *pageURL
+	}
+
+	if cachedData, found := c.cache.Get(url); found {
+		locationsResp := RespShallowLocations{}
+
+		err := json.Unmarshal(cachedData, &locationsResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+
+		return locationsResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -29,7 +40,10 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		return RespShallowLocations{}, err
 	}
 
+	c.cache.Add(url, dat)
+
 	locationsResp := RespShallowLocations{}
+
 	err = json.Unmarshal(dat, &locationsResp)
 	if err != nil {
 		return RespShallowLocations{}, err
