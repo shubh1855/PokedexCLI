3fd888f61b4e74e8016c42811d1212039aa55316
shubh1855 <paulshubhranil1855@gmail.com>
2026-05-08 13:03:36 +0530

    refactor it for map command

--- repl.go
+++ repl.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/shubh1855/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
+
+func startRepl(cfg *config) {
 	reader := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
-
-		if err := reader.Err(); err != nil {
-			fmt.Println(err)
-			return
-		}
 
 		words := cleanInput(reader.Text())
 		if len(words) == 0 {
@@ -26,14 +29,16 @@ func startRepl() {
 
 		commandName := words[0]
 
-		command, exists := commands[commandName]
+		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback()
+			err := command.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
+			continue
 		} else {
 			fmt.Println("Unknown command")
+			continue
 		}
 	}
 }
@@ -47,10 +52,8 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
-
-var commands = getCommands()
 
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
@@ -58,6 +61,16 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
 		},
 		"exit": {
 			name:        "exit",
